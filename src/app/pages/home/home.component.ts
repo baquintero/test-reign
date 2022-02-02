a544b3e21b79a60f634857d1183a101abbf39ef9
Brayan Quintero <baquintero.20@gmail.com>
2022-02-02 01:43:08 -0400

    Home Layout
    -Layout of the publication component and its respective buttons 
    -Back to top button layout and functionality for infinite scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+
+  /* VARIABLES BOOLEAN */
+  showbtn = false;
+  private scrollHeight : number = 200
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit(): void {
+
   }
+
+  /* Method to know if the user scrolls */
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+
+    const yOffSet = window.pageXOffset;
+
+    const scrollTop = this.document.documentElement.scrollTop;
+
+    this.showbtn = (yOffSet || scrollTop) > this.scrollHeight;
+  }
+
+  /* Method to comeback up page */
+  onScrollTop(): void{
+
+    this.document.documentElement.scrollTop = 0;
+  }
+
 
 }
